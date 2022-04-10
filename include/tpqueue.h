4df300715dcfa2968b495f7ff141fafabdf32524
Victoria Terganova <94222399+Terganova21@users.noreply.github.com>
2022-04-10 19:22:25 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -9,8 +9,8 @@ class TPQueue {
     T arr[100];
     int first;
     int last;
-    
-public:
+
+ public:
     TPQueue() : first(0), last(0) {}
     void push(T x) {
         if ((last - first) >= size) {
