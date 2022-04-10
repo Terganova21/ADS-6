e59b4d4cf5d0fed129cbe93b0c567b6e104eaf82
Victoria Terganova <94222399+Terganova21@users.noreply.github.com>
2022-04-10 18:51:34 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,39 +5,35 @@
 template<typename T, int size>
 class TPQueue {
 private:
-     T arr[size];
-     int first;  
-     int last; 
-public:
-     TQueue():first(0), last(0) { }
-     void push(T x) {
-         if(last - first >= size)
-             throw std::string("Full!");
-         else {
-             arr[(last++) % size]=x;
-             } else {
-             int k = last + 1;
-             while (k <= first && x.prior < arr[k % size].prior) {
-           arr[(k - 1) % size] = arr[k % size];
-           k++;
-         }
-         arr[(k - 1) % size] = x;
-         last--;
-     }
-     T pop() {
-       return arr[(first++) % size];
-     }
-     int getSize() {
-       return (last-first);
-     }
-     T front() { 
-         return arr[first%size]; 
-     } 
-     T back() { 
-         return arr[(last-1)%size]; 
-     }
+    T arr[size];
+    int first;
+    int last;
+ public:
+    TPQueue() : first(0), last(0) {}
+    void push(T x) {
+        if (getSize() >= size)
+            throw std::string("Full!");
+        else {
+            int k = last++;
+            while ((--k >= first) && (arr[k % size].prior < x.prior)) {
+                arr[(k + 1) % size] = arr[k % size];
+            }
+            arr[(k + 1) % size] = x;
+        }
+    }
+    T pop() {
+        return arr[(first++) % size];
+    }
+    int getSize() {
+        return last - first;
+    }
+    T front() {
+        return arr[(first) % size];
+    }
+    T back() {
+        return arr[(last - 1) % size];
+    }
 };
-
 struct SYM {
   char ch;
   int prior;
